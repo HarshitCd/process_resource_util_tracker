284c7ca9f00f8c00a4b1785d23cfa7698aaa8b8e
Harshit Cd <harshitcd@gmail.com>
2024-01-05 10:55:41 +0530

    Formats get_data.py

--- get_data.py
+++ get_data.py
@@ -1,39 +1,36 @@
 import concurrent.futures as cf
-from subprocess import check_output, CalledProcessError
 import time
 
 import psutil
 import json
 
-def get_pid(name: str) ->int:
+
+def get_pid(name: str) -> int:
     for proc in psutil.process_iter():
         if name.lower() in proc.name().lower():
             return proc.pid
-        
-    return -1
-    
-    # try:
-    #     return int(check_output(["pidof",name]).decode("utf-8").split(" ")[-1].strip())
-    # except CalledProcessError: 
-    #     return -1
 
-def resource_tracker(pname:str, pid: int, samples: int = 60, interval: int = 0.5) -> dict:
+    return -1
+
+
+def resource_tracker(pname: str, pid: int, samples: int = 60, interval: int = 0.5) -> dict:
     if pid == -1:
         return {}
-    
+
     resource_data: dict = dict()
     process = psutil.Process(pid)
 
     timestamp = list()
     cpu_percent = list()
     rss_usage = list()
-    
+
     for _ in range(samples):
         timestamp.append(((time.time() * 100) // 1) / 100)
         cpu_percent.append(round(process.cpu_percent(interval=interval), 2))
         rss_usage.append(round(process.memory_percent(memtype="rss"), 2))
 
-    timestamp = [(((val - timestamp[0]) * 100) // 1) /100 for val in timestamp]
+    timestamp = [(((val - timestamp[0]) * 100) // 1) / 100
+                 for val in timestamp]
     no_of_pids = len(psutil.pids())
 
     resource_data = {
@@ -41,25 +38,28 @@ def resource_tracker(pname:str, pid: int, samples: int = 60, interval: int = 0.5
         "cpu_percent": {no_of_pids: cpu_percent},
         "rss_usage": {no_of_pids: rss_usage}
     }
-    
+
     return {pname: resource_data}
+
 
 def main():
     # process_deets: list[str] = {val.strip(): get_pid(val.strip()) for val in input("Enter the process names: ").split(",")}
     process_names: list[str] = ["python3", "node"]
-    process_deets: dict = {val.strip(): get_pid(val.strip()) for val in process_names}
+    process_deets: dict = {val.strip(): get_pid(val.strip())
+                           for val in process_names}
     resource_data: dict = dict()
 
     # print(process_deets)
 
     with cf.ThreadPoolExecutor(max_workers=5) as executor:
-        futures = [executor.submit(resource_tracker, k, v) for k, v in process_deets.items()]
+        futures = [executor.submit(resource_tracker, k, v)
+                   for k, v in process_deets.items()]
 
     for future in cf.as_completed(futures):
         resource_data = {**resource_data, **future.result()}
 
     print(json.dumps(resource_data))
-    
+
 
 if __name__ == '__main__':
     main()
